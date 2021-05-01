b8e1efa5a0d1bed01984f73dcabb95b98e712797
David Bolotaev <bolotaev-david@mail.ru>
2021-05-01 23:14:07 +0300

    Создание проекта, добавление цветов, изображений фруктов

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Давид on 01.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
