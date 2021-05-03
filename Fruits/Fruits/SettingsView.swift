157395590675b587ac9722c827989f13ac6e98c4
David Bolotaev <bolotaev-david@mail.ru>
2021-05-03 12:24:53 +0300

    Add section 2 in settings

--- Fruits/Fruits/SettingsView.swift
+++ Fruits/Fruits/SettingsView.swift
@@ -11,14 +11,16 @@ struct SettingsView: View {
 	// MARK: - PROPERTIES
 
 	@Environment(\.presentationMode) var presentationMode
+	@AppStorage("isOnBoarding") var isOnBoarding = false
 
 	// MARK: - BODY
 	var body: some View {
 		NavigationView {
 			ScrollView(.vertical, showsIndicators: false) {
 				VStack(spacing: 20) {
+
 					// MARK: - SECTION 1
-					
+
 					GroupBox(label:
 								SettingsLabelView(labelText: "Fruits",
 												  labelImage: "info.circle")
@@ -34,8 +36,40 @@ struct SettingsView: View {
 								.font(.footnote)
 						}
 					}
+
 					// MARK: - SECTION 2
+
+					GroupBox(label: SettingsLabelView(labelText: "Customization",
+													  labelImage: "paintbrush")) {
+						Divider().padding(.vertical, 4)
+						Text("If you wish, you can restart the application by toggle the switch in this box. It starts the onboarding process and you will see the welcome screen again.")
+							.padding(.vertical, 8)
+							.frame(minHeight: 80)
+							.layoutPriority(1)
+							.font(.footnote)
+							.multilineTextAlignment(.leading)
+
+						Toggle(isOn: $isOnBoarding) {
+							if isOnBoarding {
+								Text("Restarted".uppercased())
+									.fontWeight(.bold)
+									.foregroundColor(.green)
+							} else {
+								Text("Restart".uppercased())
+									.fontWeight(.bold)
+									.foregroundColor(.secondary)
+							}
+						}
+						.padding()
+						.background(
+							Color(UIColor.tertiarySystemBackground)
+								.clipShape(RoundedRectangle(cornerRadius: 8,
+															style: .continuous))
+						)
+					}
+
 					// MARK: - SECTION 3
+
 					GroupBox(label: SettingsLabelView(labelText: "Application",
 													  labelImage: "apps.iphone")) {
 						SettingsRowView(name: "Developer",
